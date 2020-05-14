94a5f4229891eec4ef15718a9524d73c189408cb
Weverton Couto <wevertoncristais@gmail.com>
2020-05-14 09:48:24 -0300

    Typography

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
 import Header from '../../Components/Header/Header';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    titulo: {
+        textAlign: "center",
+        color: 'blue'
+    }
+});
 
 const Sobre = () => {
+    const classes = useStyles();
+
     return (
         <Fragment>
-            <Header />
-            <div className="container">
-                <h1>Sobre</h1>
-            </div>
+            <Header/>
+            <Container maxWidth='sm'>
+                <Typography className={classes.titulo} variant='h1' component='h2'>Sobre</Typography>
+                <Typography variant='body1' component='p'>
+                A casa do código é uma editora que desenvolve e edita livros de diversos formatos
+                </Typography>
+            </Container>
         </Fragment>
     );
 }
